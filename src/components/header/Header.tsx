9e447cb42bb6aa278236c6a0ae7b452d7b600266
Bohdan Grachov <bgrachov.94@gmail.com>
2024-09-23 12:38:58 +0300

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+// src/components/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => {
+  return <header><h1>My TypeScript React App</h1></header>;
+};
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// git add .
+// git commit -m "description of changes"
+// git push origin master
+// npm run build
+// npm run deploy
